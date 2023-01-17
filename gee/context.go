01a8aa1e7380d121ee0ee53bd8554b82f25e0602
Wang-Ji20 <824466875@qq.com>
2023-01-17 23:27:19 +0800

    feat: tries

--- gee/context.go
+++ gee/context.go
@@ -12,8 +12,11 @@ type H map[string]interface{}
 type Context struct {
 	Writer http.ResponseWriter
 	Req *http.Request
+
 	Path string
 	Method string
+	Params map[string]string
+
 	StatusCode int
 }
 
@@ -32,6 +35,11 @@ func (c *Context) PostForm(key string) string {
 
 func (c *Context) Query(key string) string {
 	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
 }
 
 func (c *Context) Status(code int) {
